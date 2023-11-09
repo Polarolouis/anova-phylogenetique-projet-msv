e83e82554e9d7a79223b38ec81f4fcb0fc504273
Polarolouis <louis.lacoste@hotmail.fr>
2023-11-09 17:47:34 +0100

    Added multiple BM generation

--- simulations/mouvement_brownien.R
+++ simulations/mouvement_brownien.R
@@ -15,10 +15,28 @@ generate_brownian_motion_traj <- function(N, time_delta, variance = 1) {
     return(data.frame(time_serie = time_serie, bm_traj = bm_traj))
 }
 
-bm_traj_and_time <- generate_brownian_motion_traj(N, time_delta)
+bm_traj_and_time <- generate_brownian_motion_traj(N, time_delta, 1)
+
+# Plotting
+ggplot(bm_traj_and_time) +
+    aes(x = time_serie, y = bm_traj) +
+    geom_hline(aes(yintercept = 0, color = "red")) +
+    geom_line()
 
 # Generate multiple BM
+n <- 100
+multiple_BM <- do.call(rbind, lapply(seq(1,n,1), function(idx) {
+    data <- generate_brownian_motion_traj(N, time_delta)
+    data$id <- rep(idx, N)
+    data <- data[,c(3,1:2)]
+    return(data)
+}))
 
+## Plotting multiple BMs
+ggplot(multiple_BM) +
+    aes(x = time_serie, y = bm_traj, group = as.factor(id), alpha = 0.5) +
+    geom_hline(aes(yintercept = 0, color = "red")) +
+    geom_line()
 
 # For phylogenic tree
 
