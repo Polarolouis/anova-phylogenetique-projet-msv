aa13972a3dbe69d2692adebed19aace07319d62e
Polarolouis <louis.lacoste@hotmail.fr>
2024-02-06 18:48:32 +0100

    Adding what to do in realTreeMethod

--- R/realTreeMethod.R
+++ R/realTreeMethod.R
@@ -1,2 +1,10 @@
 # TODO appliquer Satterthwaite, et calcule les pvalues pour les 5000 genes
 # avec Correction Bonferroni et Benjamini-Hochberg (voir Livre Christophe Giraud)
+
+# Repartir du fichier d'analyse Rmd
+# Utiliser data.trans, ligne 883 voir RMD
+
+# Appliquer notre méthode autant de fois que de gène et corriger les pvalues 
+# obtenues par la correction pour obtenir
+
+# Vérifier que la F stat = T stat ^ 2
