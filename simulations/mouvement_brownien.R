29a937f52d41afeaeba4a4e9025980d8eb261a96
Polarolouis <louis.lacoste@hotmail.fr>
2024-03-12 17:17:53 +0100

    Debut traavail fonction génération arbre phylo selon BM

--- simulations/mouvement_brownien.R
+++ simulations/mouvement_brownien.R
@@ -25,10 +25,10 @@ ggplot(bm_traj_and_time) +
 
 # Generate multiple BM
 n <- 100
-multiple_BM <- do.call(rbind, lapply(seq(1,n,1), function(idx) {
+multiple_BM <- do.call(rbind, lapply(seq(1, n, 1), function(idx) {
     data <- generate_brownian_motion_traj(N, time_delta)
     data$id <- rep(idx, N)
-    data <- data[,c(3,1:2)]
+    data <- data[, c(3, 1:2)]
     return(data)
 }))
 
@@ -40,6 +40,61 @@ ggplot(multiple_BM) +
 
 # For phylogenic tree
 
-generate_phylo_tree <- function(n_tips, max_time) {
-    
+generate_phylo_tree <- function(n_tips, N, time_delta) {
+    # Les instants
+    time_serie <- cumsum(rep(time_delta, N))
+
+    # Instants de spéciations
+    spec_times <- sort(sample(seq(1:N - 1), n_tips - 1, replace = TRUE))
+
+    # Choisir la branche dont on se sépare, au premier choix 1 seule, au second 2
+    branche_orig_spec <- sapply(
+        seq_len(length(spec_times)),
+        function(nb_choix) sample(seq(1, nb_choix), size = 1)
+    )
+
+    # On ajoute alors la première espèce
+    spec_times <- c(0, spec_times)
+
+    # Les trajectoires
+    liste_spec_traj <- lapply(seq_len(length(spec_times)), function(idx) {
+        generate_brownian_motion_traj(
+            N = N - spec_times[idx],
+            time_delta = time_delta
+        )[["bm_traj"]]
+    })
+
+    phylo_tree_data <- data.frame(
+        id_branche = factor(),
+        time = numeric(), traj = numeric()
+    )
+
+    # Pour la construction récursive de l'arbre
+    for (idx in seq_len(length(liste_spec_traj))) {
+        # Les k pas manquants
+        k <- spec_times[idx]
+
+        if (k == 0) {
+            current_data <- data.frame(
+                id_branche = rep(
+                    idx,
+                    length(liste_spec_traj[[idx]])
+                ),
+                time = time_serie, traj = liste_spec_traj[[idx]]
+            )
+        } else {
+            traj <-
+                # On prend la dernière valeur avant div qu'on ajoute à la 
+                # trajectoire
+                base <- phylo_tree_data[which(phylo_tree_data[["id_branche"]] == branche_orig_spec[[idx]]),"traj"]
+                tail(, n = 1) + liste_spec_traj[[idx]]
+            current_data <- data.frame(
+                id_branche = rep(idx, length(liste_spec_traj[[idx]])),
+                time = time_serie[(k + 1):N], traj = traj
+            )
+        }
+
+        phylo_tree_data <- rbind(phylo_tree_data, current_data)
+    }
+    return(phylo_tree_data)
 }
